6d87543f2ebf9569bb74c74e1a077c2a6d868af4
SapiGabor <55430758+SapiGabor@users.noreply.github.com>
2019-09-26 17:38:26 +0200

    Add files via upload

---
+++ valtcsereszorzattal.cpp
@@ -0,0 +1,23 @@
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+int main()
+{
+	int a=0, b=0;
+	cout<<"Adj meg ket valtozot: \n";
+	cin>>a>>b;
+	
+	cout<<"1.valtozo = "<<a<<"\n";
+	cout<<"2.valtozo = "<<b<<"\n";
+
+	a=a*b;
+	b=a/b;
+	a=a/b;
+
+	cout<<"A csere vegen: \n";
+	cout<<"1.valtozo = "<<a<<"\n";
+	cout<<"2.valtozo = "<<b<<"\n";
+
+	return 0;
+}
